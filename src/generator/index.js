91b5e18fc5c6f7ee06892916a3c266be39c491bc
DevHars <vargassanchezfabio01@gmail.com>
2022-08-29 20:18:24 -0500

    Generator initial

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,1 +1,26 @@
-function 
+// The generator it's create with the reserve word function*
+function* gen() {
+    // yield is a result that it's want have
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const g = gen();
+// For get the first value of gen() and follow
+console.log(g.next().value); // -> 1
+console.log(g.next().value); // -> 2
+console.log(g.next().value); // -> 3
+
+console.log(g.next().value); // -> 2
+
+
+function* iterate(array) {
+    for (let value of array) {
+        yield value;
+    }
+}
+
+const it = iterate(['Fabio', 'Vargas', 'Federico', 'Marx'])
+// Aced of the element
+console.log(it.next())
